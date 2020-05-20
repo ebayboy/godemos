3553f9333e21b2148607c0e438d32303babc3d66
ebayboy <ebayboy@163.com>
2020-05-20 10:42:45 +0800

    fw

--- interface.go
+++ interface.go
@@ -2,30 +2,43 @@ package main
 
 import "fmt"
 
+/* 接口开始 ============================================ */
 type Phone interface {
 	call()
 }
 
+/* 接口结束 ===========================================  */
+
+/* ========================== 接口实现1  ==============  */
 type NokiaPhone struct {
 }
 
+// func
 func (nokiaPhone NokiaPhone) call() {
 	fmt.Println("I am Nokia, i cal call you!")
 }
 
+/* =========================接口实现2 =================== */
 type IPhone struct {
 }
 
 func (iPhone IPhone) call() {
-	fmt.Println(("I am iPhone, I can call you!"))
+	fmt.Println(("I am IPhone, I can call you!"))
 }
 
 func main() {
+	//定义一个接口
 	var phone Phone
 
+	//接口实例化1
 	phone = new(NokiaPhone)
 	phone.call()
 
+	//接口实例化2
 	phone = new(IPhone)
 	phone.call()
+
+	//空接口
+	var nullFace interface{} = 1
+	fmt.Println("nullFace=", nullFace)
 }
--- struct.go
+++ struct.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Books struct {
 	title   string
@@ -9,8 +11,22 @@ type Books struct {
 	book_id int
 }
 
-func updateBook(book *Books, author string) {
-	book.author = author
+func (book *Books) Subject() string {
+	return book.subject
+}
+
+func (book *Books) SetSubject(subject string) {
+	book.subject = subject
+}
+
+func NewBooks(title string, author string, subject string, book_id int) *Books {
+	return &Books{title: title, author: author, subject: subject, book_id: book_id}
+}
+
+//struct method
+func (book *Books) getBook() {
+	fmt.Println("getBook:")
+	fmt.Printf("titile:%s \nauthor:%s \nsubject:%s \nbook_id:%d\n", book.title, book.author, book.subject, book.book_id)
 }
 
 func printBook(book *Books) {
@@ -25,24 +41,27 @@ func main() {
 	fmt.Println("hello")
 
 	var book1 Books
-	var book2 Books
+	book2 := Books{"python book2", "fanpf", "python language", 111222}
 
-	book1.title = "go go go"
+	//new创建的不需要手动delete， 有垃圾回收机制
+	book3 := NewBooks("book3", "fanyf", "javascript", 8888)
+
+	book1.title = "go book1"
 	book1.author = "www.runoob.com"
 	book1.subject = "Go book"
 	book1.book_id = 64782
-
-	book2.title = "python book"
-	book2.author = "fan pf"
-	book2.subject = "python language"
-	book2.book_id = 1111222
 
 	printBook(&book1)
 	fmt.Println("==========================")
 	printBook(&book2)
 
 	fmt.Println("==========================")
-	updateBook(&book2, "update_author")
 	printBook(&book2)
+	book2.SetSubject("new subject")
+	fmt.Println("after SetSubject:", book2.Subject())
 
+	//call struct method
+	book1.getBook()
+	book2.getBook()
+	book3.getBook()
 }
