2a7a36ffc008f69b053178ec71f6d1681735706f
ebayboy <ebayboy@163.com>
2020-05-20 10:55:30 +0800

    fw

--- hello.go
+++ hello.go
@@ -8,4 +8,5 @@ func hello() {
 
 func main() {
 	hello()
+
 }
---
+++ slice.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//生命切片
+	s := make([]string, 5)
+
+	//初始化
+	for n := 0; n < len(s); n++ {
+		s[n] = fmt.Sprintf("name_%d", n)
+	}
+
+	//ffr 打印 key, value
+	for i, i2 := range s {
+		fmt.Printf("i:%d i2:%s\n", i, i2)
+	}
+
+}
