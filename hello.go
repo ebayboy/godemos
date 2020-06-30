cc8e8fe88fea69c4ae6026336f566a13a8b2cfc2
ebayboy <ebayboy@163.com>
2020-06-30 09:37:12 +0800

    fw

--- hello.go
+++ hello.go
@@ -1,26 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"os"
 )
 
-var debugLog *log.Logger
-
 func main() {
-
-	debug := false
-	if debug {
-		debugLog = log.New(os.Stdout, "[Debug]", log.Llongfile)
-	} else {
-		logFile, err := os.Create("/dev/null")
-		if err != nil {
-			log.Fatalln("open file error !")
-		}
-		debugLog = log.New(logFile, "[Debug]", log.Llongfile)
-		defer logFile.Close()
-	}
-
-	debugLog.Println("A debug message here")
-
+	fmt.Println("hlll")
+	log.Println("hlll")
 }
