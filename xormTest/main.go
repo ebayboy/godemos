87f6e823f22be088454a93869826bb7bf4884e18
ebayboy <ebayboy@163.com>
2021-02-02 19:03:25 +0800

    fw

--- xormTest/main.go
+++ xormTest/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/go-xorm/xorm"
 	"github.com/golang/glog"
@@ -13,7 +15,9 @@ type User struct {
 	Id       int64
 	Name     string
 	Age      int64
-	CreateAt int64 `xorm:"created"`
+	CreateAt int64     `xorm:"created"`
+	DeleteAt time.Time `xorm:"deleted"`
+	UpdateAt int64     `xorm:"updated"`
 }
 
 func main() {
@@ -46,9 +50,45 @@ func main() {
 		Age:  33,
 	}
 
+	//Insert
 	rows, err2 := engine.Insert(&u1)
 	if err2 != nil {
 		glog.Error("Error:", err2.Error())
 	}
 	glog.Info("rows:", rows)
+
+	//Delete
+	var u2 User
+	rows, err = engine.Id(8).Delete(&u2)
+	if err != nil {
+		glog.Error("Error:", err.Error())
+	}
+	glog.Info("del rows:", rows)
+
+	//Get
+	u3 := User{
+		Name: "888",
+	}
+	var found bool
+	found, err = engine.Get(&u3)
+	if err != nil {
+		glog.Error("Error:", err.Error())
+	}
+	if !found {
+		glog.Warning("not found user!")
+	} else {
+		glog.Info("found user:", u3)
+	}
+
+	//Update
+	u3.Age = 999
+	rows, err = engine.Id(u3.Id).Update(&u3)
+	if err != nil {
+		glog.Error("error:", err.Error())
+	}
+	if rows > 0 {
+		glog.Infof("update user %s ok!", u3.Name)
+	} else {
+		glog.Warningf("update user %s failed!", u3.Name)
+	}
 }
