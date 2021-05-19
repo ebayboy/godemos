efe01451e8da090414a0630b2aa85a9c0365081e
fanpengfei1 <fanpengfei@jd.com>
2021-05-19 18:29:32 +0800

    fw

--- convert2utf8/go.mod
+++ convert2utf8/go.mod
@@ -2,4 +2,7 @@ module convert2utf8
 
 go 1.16
 
-require github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca
+require (
+	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca
+)
--- convert2utf8/test.go
+++ convert2utf8/test.go
@@ -2,10 +2,52 @@ package main
 
 import (
 	"bufio"
+	"errors"
+	"fmt"
+	"github.com/axgle/mahonia"
 	"github.com/saintfish/chardet"
 	"io"
 	"os"
 )
+
+/*
+mapping:
+detector ->  decoder
+UTF-8 -> utf8
+ISO-8859-1 -> ISO-8859-1
+GB-18030 -> gb18030
+*/
+
+func Str2UTF8(src string, datautf8 string) error {
+	//建立charset识别字符集到转码字符集的映射
+	chardet_to_chardet := map[string]string{
+		"UTF-8":      "utf8",
+		"ISO-8859-1": "ISO-8859-1",
+		"GB-18030":   "gb18030",
+	}
+
+	detector := chardet.NewTextDetector()
+	charset, err := detector.DetectBest([]byte(src))
+	if err != nil {
+		return err
+	}
+
+	decCharset, exist := chardet_to_chardet[charset.Charset]
+	if !exist {
+		return errors.New(fmt.Sprintf("Error: chardet[%s] to [%s] not exit!", charset, decCharset))
+	}
+
+	d := mahonia.NewDecoder(decCharset)
+	if d == nil {
+		return errors.New(fmt.Sprintf("Error:Could not create decoder for %s convert to  %s", charset, decCharset))
+	}
+
+	datautf8 = d.ConvertString(string(src))
+
+	fmt.Printf("[%s] -> [%s] datautf8:[%s]\n", charset.Charset, decCharset, datautf8)
+
+	return nil
+}
 
 func main() {
 	//8859-1.txt
@@ -19,30 +61,14 @@ func main() {
 
 	rd := bufio.NewReader(f)
 	for {
-		line, err := rd.ReadBytes('\n') //以'\n'为结束符读入一行
+		line, err := rd.ReadString('\n') //以'\n'为结束符读入一行
 		if err != nil || io.EOF == err {
 			break
 		}
 
-		detector := chardet.NewTextDetector()
-		charset, err := detector.DetectBest(line)
-		if err != nil {
-			panic(err)
-		}
-
-		println("=====line:", string(line))
-		println("charset:", charset.Charset, " charset.Language:", charset.Language)
+		fmt.Println("=====line:", string(line))
+		datautf8 := ""
+		Str2UTF8(line, datautf8)
+		fmt.Println("Done=====datautf8:", datautf8)
 	}
-
-	return
-
-	rawBytes := []byte("some text")
-	detector := chardet.NewTextDetector()
-	charset, err := detector.DetectBest(rawBytes)
-	if err != nil {
-		panic(err)
-	}
-
-	println(charset.Charset)
-	println(charset.Language)
 }
