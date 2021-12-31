f6b24198932123598f69f6031eaec89af2dce97d
fanpengfei1 <fanpengfei@jd.com>
2021-12-31 17:49:24 +0800

    fw

--- websocketTest/client.go
+++ websocketTest/client.go
@@ -1,10 +1,3 @@
-// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-//go:build ignore
-// +build ignore
-
 package main
 
 import (
@@ -18,7 +11,7 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var addr = flag.String("addr", "localhost:8080", "http service address")
+var addr = flag.String("addr", "0.0.0.0:8000", "http service address")
 
 func main() {
 	flag.Parse()
@@ -27,21 +20,25 @@ func main() {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
-	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
-	log.Printf("connecting to %s", u.String())
+	//websocket的schema是ws, 例如:  ws://127.0.0.1:8000/
+	//url: ws://127.0.0.1:8000/echo
+	urlStr := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
+	log.Printf("connecting to %s", urlStr.String())
 
-	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	//Dial
+	conn, _, err := websocket.DefaultDialer.Dial(urlStr.String(), nil)
 	if err != nil {
 		log.Fatal("dial:", err)
 	}
-	defer c.Close()
+	defer conn.Close()
 
 	done := make(chan struct{})
 
+	//启动协程读取websocket消息
 	go func() {
 		defer close(done)
 		for {
-			_, message, err := c.ReadMessage()
+			_, message, err := conn.ReadMessage()
 			if err != nil {
 				log.Println("read:", err)
 				return
@@ -56,23 +53,29 @@ func main() {
 	for {
 		select {
 		case <-done:
+			//服务器主动关闭处理
 			return
-		case t := <-ticker.C:
-			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
+		case timeNow := <-ticker.C:
+			//每秒向websocket连接写入消息
+			err := conn.WriteMessage(websocket.TextMessage, []byte(timeNow.String()))
 			if err != nil {
 				log.Println("write:", err)
 				return
 			}
+
+			//收到中断信号, 写入websocket关闭消息
 		case <-interrupt:
+			//客户端主动关闭处理
 			log.Println("interrupt")
 
 			// Cleanly close the connection by sending a close message and then
 			// waiting (with timeout) for the server to close the connection.
-			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 			if err != nil {
 				log.Println("write close:", err)
 				return
 			}
+			//收到服务器关闭确认消息后, 1秒后退出程序
 			select {
 			case <-done:
 			case <-time.After(time.Second):
--- websocketTest/server.go
+++ websocketTest/server.go
@@ -1,14 +1,8 @@
-// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-//go:build ignore
-// +build ignore
-
 package main
 
 import (
 	"flag"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -16,25 +10,31 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var addr = flag.String("addr", "localhost:8080", "http service address")
+var srvAddr = flag.String("srvAddr", "0.0.0.0:8000", "http service srvAddress")
 
 var upgrader = websocket.Upgrader{} // use default options
 
+//重点关注函数
 func echo(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
+
+	fmt.Println("====Enter echo ...")
+	fmt.Printf("request:[%v]\n", *r)
+
+	//update http to websocket
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Print("upgrade:", err)
 		return
 	}
-	defer c.Close()
+	defer conn.Close()
 	for {
-		mt, message, err := c.ReadMessage()
+		mt, message, err := conn.ReadMessage()
 		if err != nil {
 			log.Println("read:", err)
 			break
 		}
 		log.Printf("recv: %s", message)
-		err = c.WriteMessage(mt, message)
+		err = conn.WriteMessage(mt, message)
 		if err != nil {
 			log.Println("write:", err)
 			break
@@ -43,17 +43,40 @@ func echo(w http.ResponseWriter, r *http.Request) {
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("====Enter home:")
+	fmt.Printf("request:[%v]\n", *r)
+
+	//跳转到websocket的echo
 	homeTemplate.Execute(w, "ws://"+r.Host+"/echo")
 }
 
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
+
+	//建立路由， 也key用mux开源模块
+
+	//作用：通过client.go 访问这个路由
+	//客户端连接的是ws://127.0.0.1:8000/echo , 这里的路由是http://127.0.0.1:8000/echo, 怎么联通的？
+	//使用ws://实际上也是走的http协议，只是请求头有变化
+	//websocket 也是走的http 协议，只是一些头部字段有区别， 例如：
+	//request:[{GET /echo HTTP/1.1 1 1 map[Connection:[Upgrade] Sec-Websocket-Key:[xJuxUVPvz97A1tt+8Fz8Eg==] Sec-Websocket-Version:[13] Upgrade:[websocket] User-Agent:[Go-http-client/1.1]] {} <nil> 0 [] false 0.0.0.0:8000 map[] map[] <nil> map[] 127.0.0.1:36824 /echo <nil> <nil> <nil> 0xc0000b43c0}]
+	//所以到echo的是http,进入echo函数内部会将http请求升级为websocket请求
+
 	http.HandleFunc("/echo", echo)
+
+	//作用：通过浏览器访问/， 跳转到echo
 	http.HandleFunc("/", home)
-	log.Fatal(http.ListenAndServe(*addr, nil))
+
+	//开启http监听
+	fmt.Println("http.ListenAndServe:", *srvAddr)
+	err := http.ListenAndServe(*srvAddr, nil)
+	if err != nil {
+		log.Fatalf("Error:%v\n", err.Error())
+	}
 }
 
+//http 使用websocket通信
 var homeTemplate = template.Must(template.New("").Parse(`
 <!DOCTYPE html>
 <html>
