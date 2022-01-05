8db5983480607330f83cadc102c8c0639724a189
fanpengfei1 <fanpengfei@jd.com>
2022-01-05 12:34:13 +0800

    fw

---
+++ grpc_hello/hello/client/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+
+	pb "github.com/godemos/grpc_hello/proto/hello"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
+)
+
+// 实现rpc hello方法服务接口
+
+const (
+	Address = "127.0.0.1:8000"
+)
+
+func main() {
+	//create grpc conn
+	conn, err := grpc.Dial(Address, grpc.WithInsecure())
+	if err != nil {
+		grpclog.Fatalln(err)
+	}
+	defer conn.Close()
+
+	//使用conn创建客户端
+	c := pb.NewHelloClient(conn)
+
+	//调用方法
+	req := &pb.HelloRequest{Name: "gRPC"}
+	res, err := c.SayHello(context.Background(), req)
+	if err != nil {
+		grpclog.Fatalln(err)
+	}
+	grpclog.Println(res.Message)
+}
---
+++ grpc_hello/hello/server/start.sh
@@ -0,0 +1,3 @@
+
+export GRPC_GO_LOG_SEVERITY_LEVEL=INFO
+./server
