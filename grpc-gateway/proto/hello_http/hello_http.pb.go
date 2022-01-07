dddc1c60e6e341015237bc0e0332c5675f2bb600
fanpengfei1 <fanpengfei@jd.com>
2022-01-07 14:51:40 +0800

    fw

--- grpc-gateway/proto/hello_http/hello_http.pb.go
+++ grpc-gateway/proto/hello_http/hello_http.pb.go
@@ -8,7 +8,7 @@ package hello_http
 
 import (
 	context "context"
-	_ "github.com/jergoo/go-grpc-example/proto/google/api"
+	_ "github.com/godemos/grpc-gateway/proto/google/api"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
