1b7361798836239d0d2dd25967ac2acca8a09494
fanpengfei1 <fanpengfei@jd.com>
2023-06-21 04:00:15 +0000

    fw

--- websocketTest/curl_133.8.sh
+++ websocketTest/curl_133.8.sh
@@ -8,6 +8,12 @@
 #   "x-org-scheme": []string{"http"},
 
 # curl http://10.226.133.8:28080 -H'x-cg-id: cg-llm8mksvcd' https://www.baidu.com/ RespCode:200
-HOST="http://0.0.0.0:18000"
-curl --verbose "$HOST/echo_once" -H 'Upgrade: websocket' -H 'Connection: Upgrade' -H 'Sec-WebSocket-Version: 13' -H 'Sec-webSocket-Key: eeZn6lg/rOu8QbKwltqHDA==' -H 'x-cg-id: cg-llm8mksvcd' -H 'x-org-host: 127.0.0.1:8000' -H 'x-org-scheme: http' 
+#HOST="http://0.0.0.0:18000"
+HOST="http://10.226.133.8:28080"
+ORGHOST="0.0.0.0:8000"
+#ORGHOST="www.baidu.com"
+
+curl --verbose "$HOST/echo_once" -H 'Upgrade: websocket' -H 'Connection: Upgrade' -H 'Sec-WebSocket-Version: 13' -H 'Sec-webSocket-Key: eeZn6lg/rOu8QbKwltqHDA==' -H 'x-cg-id: cg-llm8mksvcd' -H "x-org-host: $ORGHOST" -H 'x-org-scheme: http' 
+
+#curl "$HOST" -H 'x-cg-id: cg-llm8mksvcd' -H "x-org-host: $ORGHOST" -H 'x-org-scheme: http' 
 
